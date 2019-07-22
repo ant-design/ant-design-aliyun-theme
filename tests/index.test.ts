4030325002a6444c1b9d137fcfa2184daf707e3a
afc163 <afc163@gmail.com>
2019-07-22 16:00:26 +0800

    :white_check_mark: fix eslint

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,22 @@
+/* eslint @typescript-eslint/explicit-function-return-type: 0 */
 import less from 'less';
 import aliyunTheme from '..';
 
-interface Vars { [key: string]: string };
+interface Vars {
+  [key: string]: string;
+}
 
 describe('ant-design-aliyun-theme', () => {
   it('less compile without errors', () => {
-    less.render('', {
-      modifyVars: aliyunTheme as Vars,
-    }).catch(e => {
-      console.error(e);
-      expect(() => {
-        throw e;
-      }).not.toThrow();
-    });
+    less
+      .render('', {
+        modifyVars: aliyunTheme as Vars,
+      })
+      .catch(e => {
+        console.error(e);
+        expect(() => {
+          throw e;
+        }).not.toThrow();
+      });
   });
 });
